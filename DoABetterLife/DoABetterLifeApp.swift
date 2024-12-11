e6104ecd3b314a1ece3668c3f16aeeb8ff8d1a69
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2024-12-11 17:39:45 -0300

    Project created.

---
+++ DoABetterLife/DoABetterLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoABetterLifeApp.swift
+//  DoABetterLife
+//
+//  Created by Bruno Duarte on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DoABetterLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DoABetterLifeTests/DoABetterLifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DoABetterLifeTests.swift
+//  DoABetterLifeTests
+//
+//  Created by Bruno Duarte on 11/12/24.
+//
+
+import Testing
+@testable import DoABetterLife
+
+struct DoABetterLifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
